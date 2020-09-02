61b53a1ec4350d4b9ee8c80d4a5652a5d5c2db47
benkemad <70264888+benkemad@users.noreply.github.com>
2020-09-02 14:23:20 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -148,6 +148,255 @@ apt-get -y install squid3
 wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/benkemad/benninstall/master/squid3.conf"
 sed -i $MYIP2 /etc/squid/squid.conf
 
+# install openvpn
+apt-get install -y openvpn easy-rsa iptables openssl ca-certificates gnupg
+apt-get install -y net-tools
+cp -r /usr/share/easy-rsa /etc/openvpn
+cd /etc/openvpn
+cd easy-rsa
+
+
+cp openssl-1.0.0.cnf openssl.cnf
+source ./vars
+./clean-all
+source vars
+rm -rf keys
+./clean-all
+./build-ca
+./build-key-server server
+./pkitool --initca
+./pkitool --server server
+./pkitool client
+./build-dh
+cp keys/ca.crt /etc/openvpn
+cp keys/server.crt /etc/openvpn
+cp keys/server.key /etc/openvpn
+cp keys/dh2048.pem /etc/openvpn
+cp keys/client.key /etc/openvpn
+cp keys/client.crt /etc/openvpn
+
+echo 'port 1194
+proto tcp
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh2048.pem
+persist-key
+persist-tun
+keepalive 10 120
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
+client-cert-not-required
+username-as-common-name
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+comp-lzo
+status server-tcp-1194.log
+verb 3' >/etc/openvpn/server-tcp-1194.conf
+
+echo 'port 9994
+proto tcp
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh2048.pem
+persist-key
+persist-tun
+keepalive 10 120
+server 10.9.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
+client-cert-not-required
+username-as-common-name
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+comp-lzo
+status server-tcp-9994.log
+verb 3' >/etc/openvpn/server-tcp-9994.conf
+
+echo 'port 25000
+proto udp
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh2048.pem
+persist-key
+persist-tun
+keepalive 10 120
+server 20.8.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
+client-cert-not-required
+username-as-common-name
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+comp-lzo
+status server-udp-25000.log
+verb 3' >/etc/openvpn/server-udp-25000.conf
+
+systemctl enable openvpn
+service openvpn restart
+
+cd
+
+# * Membuat Config OpenVPN * #
+
+echo "client
+dev tun
+proto tcp
+remote $MYIP 1194
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+duplicate-cn
+comp-lzo
+verb 3
+
+" >/var/www/html/client-tcp-1194.ovpn
+
+echo "client
+dev tun
+proto tcp
+remote $MYIP 9994
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+duplicate-cn
+comp-lzo
+verb 3
+
+" >/var/www/html/client-tcp-9994.ovpn
+
+echo "client
+dev tun
+proto udp
+remote $MYIP 25000
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+duplicate-cn
+comp-lzo
+verb 3
+
+" >/var/www/html/client-udp-25000.ovpn
+
+echo "client
+dev tun
+proto tcp
+remote $MYIP 2905
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+duplicate-cn
+comp-lzo
+verb 3
+
+" >/var/www/html/client-ssl-2905.ovpn
+
+echo "client
+dev tun
+proto tcp
+remote $MYIP 9443
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+duplicate-cn
+comp-lzo
+verb 3
+
+" >/var/www/html//var/www/html/client-ssl-9443.ovpn
+
+echo "client
+dev tun
+proto tcp
+remote $MYIP 1194
+http-proxy $MYIP 8080
+http-proxy-option CUSTOM-HEADER CONNECT HTTP/1.1
+http-proxy-option CUSTOM-HEADER Host m.instagram.com
+http-proxy-option CUSTOM-HEADER X-Online-Host m.instagram.com
+http-proxy-option CUSTOM-HEADER X-Forward-Host m.instagram.com
+http-proxy-option CUSTOM-HEADER Connection Keep-Alive
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+duplicate-cn
+comp-lzo
+verb 3
+
+" >/var/www/html/instagram.ovpn
+
+
+cd
+
+apt-get install -y zip
+cd /var/www/html
+
+# input ca
+{
+echo "<ca>"
+cat "/etc/openvpn/ca.crt"
+echo "</ca>"
+} >>client-tcp-1194.ovpn
+
+{
+echo "<ca>"
+cat "/etc/openvpn/ca.crt"
+echo "</ca>"
+} >>client-tcp-9994.ovpn
+
+{
+echo "<ca>"
+cat "/etc/openvpn/ca.crt"
+echo "</ca>"
+} >>client-ssl-9443.ovpn
+
+{
+echo "<ca>"
+cat "/etc/openvpn/ca.crt"
+echo "</ca>"
+} >>client-ssl-2905.ovpn
+
+{
+echo "<ca>"
+cat "/etc/openvpn/ca.crt"
+echo "</ca>"
+} >>client-udp-25000.ovpn
+
+{
+echo "<ca>"
+cat "/etc/openvpn/ca.crt"
+echo "</ca>"
+} >>instagram.ovpn
+
+
+# zip config
+zip client-config.zip client-tcp-1194.ovpn client-tcp-9994.ovpn client-ssl-9443.ovpn client-ssl-2905.ovpn client-udp-25000.ovpn instagram.ovpn
+
 # setting vnstat
 apt-get -y update;apt-get -y install vnstat;vnstat -u -i eth0;service vnstat restart 
 

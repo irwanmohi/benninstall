09dbde40572647e579ed88c7b0a549a9384c9772
benkemad <70264888+benkemad@users.noreply.github.com>
2020-10-14 20:00:54 +0700

    Update speedtest_cli.py

--- speedtest_cli.py
+++ speedtest_cli.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-sudo apt-get -y install gnupg1 apt-transport-https dirmngr
+apt-get -y install gnupg1 apt-transport-https dirmngr
 export INSTALL_KEY=379CE192D401AB61
 export DEB_DISTRO=$(lsb_release -sc)
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
-echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
-sudo apt-get update
-sudo apt-get -y install speedtest
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | tee  /etc/apt/sources.list.d/speedtest.list
+apt-get update
+apt-get -y install speedtest

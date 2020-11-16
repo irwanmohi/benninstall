4bd40dcde78fad17ba6c0c6361fa5c853a266057
benkemad <70264888+benkemad@users.noreply.github.com>
2020-11-16 14:33:31 +0700

    Update wgmenu.sh

--- wgmenu.sh
+++ wgmenu.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#
-# https://github.com/Nyr/wireguard-install
-#
-# Copyright (c) 2020 Nyr. Released under the MIT License.
 
 
 # Detect Debian users running the script with "sh" instead of bash

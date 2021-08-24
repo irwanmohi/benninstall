890d797a67f3b9cd572f4e205c98a9150964a64a
benkemad <70264888+benkemad@users.noreply.github.com>
2021-08-24 22:36:20 +0700

    Delete contact.sh

--- contact.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Contact Me
-echo -e "   ======================"
-echo -e "   |   Contact Author   |"
-echo -e "   ----------------------"
-echo -e ""
-echo -e "   Telegram  : t.me/Kemaddd"
-echo -e "   WhatsApp  : +62 812 1636 5957"
-echo -e "   Instagram : @ihzarafi11"

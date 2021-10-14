5359606baf65eb41bd2708085cc6f876e7f4fd10
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-15 02:42:18 +0700

    Update sstp.sh

--- sstp.sh
+++ sstp.sh
@@ -41,7 +41,7 @@ make
 cpack -G DEB
 dpkg -i accel-ppp.deb
 mv /etc/accel-ppp.conf.dist /etc/accel-ppp.conf
-wget -O /etc/accel-ppp.conf "https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/accel.conf"
+wget -O /etc/accel-ppp.conf "https://raw.githubusercontent.com/benkemad/benninstall/master/accel.conf"
 sed -i $MYIP2 /etc/accel-ppp.conf
 chmod +x /etc/accel-ppp.conf
 systemctl start accel-ppp

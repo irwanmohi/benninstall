6dc2de021113239e69a4baa46596d731c760b429
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-15 19:14:57 +0700

    Update addhost.sh

--- addhost.sh
+++ addhost.sh
@@ -1,3 +1,14 @@
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
 mkdir /var/lib/premium-script;
 echo "Enter the VPS Subdomain Hostname"
 read -p "Hostname / Domain: " host

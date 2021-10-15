6a6ff2952f42a38ba7bc57da403cb2f4bf07fa6c
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-15 22:17:00 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
 IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
-if [ $MYIP == $IZIN ]; then
+if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
 echo -e "${green}Permission Accepted...${NC}"

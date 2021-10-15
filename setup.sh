5509a664c787aec3d9d835092b6e03215271ab25
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-15 21:12:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,10 +12,15 @@ NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
 IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
+if [ $MYIP == $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
 echo -e "${green}Permission Accepted...${NC}"
+fi
+else if [ $MYIP != $IZIN ]; then
+echo -e "${error}Permission Denied...${NC}"
+else
+echo -e "${error}Permission Denied...${NC}"
 fi
 mkdir /var/lib/premium-script;
 echo "Enter the VPS Subdomain Hostname, if not available, please click Enter"

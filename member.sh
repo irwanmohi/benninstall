ca94079e069854c94b1f41a48251822530d9a514
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-16 21:36:45 +0700

    Update member.sh

--- member.sh
+++ member.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
+sleep 0.5
 echo "-------------------------------"
 echo "USERNAME          EXP DATE     "
 echo "-------------------------------"
@@ -16,4 +28,4 @@ JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)
 echo "-------------------------------"
 echo "Jumlah akun: $JUMLAH user"
 echo "-------------------------------"
-echo -e "by LostServer"
+echo -e ""

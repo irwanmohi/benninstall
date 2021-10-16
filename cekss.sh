748e96cd3381a3ad03f1a430f287aa700cd880b3
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-16 21:30:23 +0700

    Update cekss.sh

--- cekss.sh
+++ cekss.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
+sleep 0.5
 echo "-------------------------------";
 echo "---=[ SS - OBFS User Login ]=---";
 echo "-------------------------------";
@@ -31,6 +43,6 @@ echo " $akun - $port"
 echo "$jum"; x=$(( "$x" + 1 ))
 echo "-------------------------------"
 done
-echo -e "By LostServer"
+echo -e ""
 
 

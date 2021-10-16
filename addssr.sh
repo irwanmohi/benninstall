fb57ab0b02494cfc5f1b2713c0bf7b0dddd6c299
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-16 21:04:17 +0700

    Update addssr.sh

--- addssr.sh
+++ addssr.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
+sleep 0.5
 IP=$(wget -qO- icanhazip.com);
 echo "Please enter the username you want to set (do not repeat, does not support Chinese, will be reported incorrect!)"
 read -e -p "(Default: ):" ssr_user

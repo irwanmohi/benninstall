ddc64b098197d278af3fcaf8ef0586a1fc0df859
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-16 21:32:26 +0700

    Update cektr.sh

--- cektr.sh
+++ cektr.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
+sleep 0.5
 data=( `cat /etc/v2ray/trojan.json | grep '^###' | cut -d ' ' -f 2`);
 echo "-------------------------------";
 echo "-----=[ Trojan User Login ]=-----";
@@ -29,5 +42,5 @@ echo "$jum2";
 echo "-------------------------------"
 fi
 done
-echo "By LostServer"
+echo ""
 

d02d3243872201759415c88ff3ae9a64255d14de
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-16 21:20:57 +0700

    Update cek.sh

--- cek.sh
+++ cek.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# Script by : LostServer
 clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
+sleep 0.5
 echo " "
 echo " "
 
@@ -72,5 +83,5 @@ echo " Multi Login = kill "
 echo " Cara pakai : kill [nombor PID] "
 echo "-------------------------------------"
 
-echo " By LostServer"
+echo ""
 echo "";

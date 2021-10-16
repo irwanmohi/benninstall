196aeb27cb57ecc15a17b21e350ad82f9fb273e9
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-16 21:33:16 +0700

    Update cekvless.sh

--- cekvless.sh
+++ cekvless.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
+sleep 0.5
 echo -n > /tmp/other.txt
 data=( `cat /etc/v2ray/vless.json | grep '^###' | cut -d ' ' -f 2`);
 echo "-------------------------------";

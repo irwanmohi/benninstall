a74ea8d1bbe7a7ec88da8dd29cfdebc0b7b8482e
benkemad <70264888+benkemad@users.noreply.github.com>
2021-10-16 21:27:36 +0700

    Update ceklim.sh

--- ceklim.sh
+++ ceklim.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/benkemad/benninstall/master/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
+fi
+sleep 0.5
 echo " "
 echo "===========================================";
 echo " ";
@@ -17,5 +29,5 @@ echo " Skrip User-Limit belum dijalankan"
 fi
 echo " ";
 echo "===========================================";
-echo "By LostServer";
+echo "";
 echo " ";
